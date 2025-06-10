b75c484a3d6357516f93d9cd5694fd369fdcaf3f
Banda Sai Prashanth <65449720+Sai-Prashanth-Banda@users.noreply.github.com>
2025-06-09 20:16:06 -0500

    Fix Vite config plugin import paths

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ const isDev = process.env.NODE_ENV !== 'production';
 let inlineEditPlugin, editModeDevPlugin;
 
 if (isDev) {
-	inlineEditPlugin = (await import('./plugins/visual-editor/vite-plugin-react-inline-editor.js')).default;
-	editModeDevPlugin = (await import('./plugins/visual-editor/vite-plugin-edit-mode.js')).default;
+	inlineEditPlugin = (await import('./vite-plugin-react-inline-editor.js')).default;
+	editModeDevPlugin = (await import('./vite-plugin-edit-mode.js')).default;
 }
 
 const configHorizonsViteErrorHandler = `
